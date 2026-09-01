2bbb360145b8adba4397d5bbab4bee89c0d8b575
AnishGupta77 <anish.chandra01@gmail.com>
2026-09-01 11:34:24 +0530

    Update commonstepdefinitions.java

--- src/test/java/com/dummyjson/stepdefinitions/commonstepdefinitions.java
+++ src/test/java/com/dummyjson/stepdefinitions/commonstepdefinitions.java
@@ -87,7 +87,7 @@ public class commonstepdefinitions{
                 testData.get("expectedStatusCode").asInt();
 
         testContext.setExpectedStatusCode(expectedStatusCode);
-        System.out.println("added product from Anishtestdata");
+        System.out.println("added product from MergeconflictAnishtestdata");
     }
 
     @When("I send the request to add the product")
